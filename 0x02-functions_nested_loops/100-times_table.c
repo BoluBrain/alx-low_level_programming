130bcfa35d8b00f6a5fea2c357eee6571eaba521
BoluBrain <hamboluwatife37@gmail.com>
2022-09-13 17:08:13 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,41 +22,33 @@ void three_dig(int n)
  *
  * Return: nothing
  */
-void print_times_table(int n
-		{
-		int i, j, product;
+void print_times_table(int n)
+{
+	int i, j, product;
 
-		if (n >= 0 && n <= 15)
-		{
+	if (n >= 0 && n <= 15)
+	{
 		for (i = 0; i <= n; i++)
 		{
-		for (j = 0; j <= n; j++)
-		{
-		product = i * j;
-		if (product > 99)
-		{
-		three_dig(product);
+			for (j = 0; j <= n; j++)
+			{
+				product = i * j;
+				if (product > 99)
+				{
+					three_dig(product);
+				}
+				else if (product / 10 > 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product / 10 + '0');
+					_putchar((product) % 10 + '0');
+				}
+				else
+					_putchar(product % 10 + '0');
+			}
+			_putchar('\n');
 		}
-		else if (product / 10 > 0)
-		{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(product / 10 + '0');
-		_putchar((product) % 10 + '0');
-		}
-		else if (j != 0)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(product % 10 + '0');
-		}
-		else
-			_putchar(product % 10 + '0');
-		}
-		_putchar('\n');
-		}
-		}
-		}
+	}
+}
